eef46583326579e1c583a0bc05e6a15c3e1cd1c3
Nils Kruse <nilskruse97@gmail.com>
2020-07-18 11:43:28 +0200

    dl script

--- arch-installscript.sh
+++ arch-installscript.sh
@@ -1,1 +1,4 @@
-
+#!/bin/bash
+curl -LJO https://raw.githubusercontent.com/nilskruse97/arch-install-script/master/script2.sh
+curl -LJO https://raw.githubusercontent.com/nilskruse97/arch-install-script/master/script3.sh
+curl -LJO https://raw.githubusercontent.com/nilskruse97/arch-install-script/master/testpart.sh
